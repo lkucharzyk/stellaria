0fbe1c73c16521b95fe03aaa5ec65389ce54f703
Łukasze <lukaszkucharzyk17@gmail.com>
2023-08-16 18:32:40 +0200

    small fixex ti title screen and moon animation

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ window.mobileCheck = function() {
     return check;
   };
 
- const przyspiesz = 1.7
+ const przyspiesz = 11.7
 
 class Plant{
     constructor(){
@@ -99,7 +99,7 @@ class Habitat{
         this.weather = 'rainy';
 
         this.waterLevel = -2;
-        this.minWeterLevel = -63;
+        this.minWeterLevel = -2//-63;
 
        this.dayInterval =setInterval( () =>this._dayPass(), 1000 /przyspiesz ); // one day - 1s
     }
@@ -715,7 +715,7 @@ class Canvas{
     }
 
     drawMoon(){
-        const totalFrames = 16;
+        const totalFrames = 15;
         const frame = (habitat.day/habitat.maxDay * totalFrames).toFixed(0); 
         const width = 20;
         const height = 16;
